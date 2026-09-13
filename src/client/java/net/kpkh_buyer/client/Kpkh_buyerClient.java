d72e5274a6a418f36bf71016f4eab43275667636
kpaakeh <100779868+kpaakeh@users.noreply.github.com>
2026-09-13 22:42:15 +0300

    first commit

---
+++ src/client/java/net/kpkh_buyer/client/Kpkh_buyerClient.java
@@ -0,0 +1,10 @@
+package net.kpkh_buyer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Kpkh_buyerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
